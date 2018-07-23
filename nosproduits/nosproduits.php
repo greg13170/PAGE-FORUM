7ba074357d0af2b6d33ee4a0d79ca5f2e31095eb
greg13170 <39373118+greg13170@users.noreply.github.com>
2018-07-23 16:21:56 +0200

    Add files via upload

---
+++ nosproduits/nosproduits.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+ <html>
+     <head>
+     
+     <meta charset="utf-8">
+         
+         <meta name="viewport" content="">
+         <link rel="stylesheet" type="text/css" href="nosproduits.css">
+         <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+         <link href="https://fonts.googleapis.com/css?family=Cinzel|Noticia+Text" rel="stylesheet"> 
+          
+     
+</head>
+
+
+<body>
+        
+
+
+     <div class="navbaraccueil">
+                
+             <a class="active" href="#">CONTACT</a>
+                <a href="#">SERVICES</a>
+                  <a href="#">PRODUITS</a>
+                    <a href="#">ACCUEIL</a>
+                
+     </div>
+					   
+					   
+	<div class= "texthaut ">
+	
+    <h4>Nos produits :</h4>
+	<b>En provenance de l'Europe toute entière<br>
+et regroupant des produits locaux,<br>
+nos fromages sont de qualité<br> et
+vous feront voyager dans 
+les pays du continent
+	</b>
+	</div>
+    
+	<div class= "textbas">
+		<h4>Nos types de fromages :</h4>
+		<b>Nous disposons de tout type de produit,<br>
+      en partant du fromage de brebis au<br>
+           fromage de chèvres ainsi que du <br>
+                        fromage au lait de vache.</b>
+	
+	
+    // police = Cinzel , #FB8B0E (orange), background black
+      
+            
+
+     
+     </body>
+
+</html>
